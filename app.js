48c8d1f94e87f9d6ed0f5ccf0b0a9190dfc448c4
Rasl0406 <durjoychowdhury0406@gmail.comm>
2022-10-09 17:42:14 +0600

    pinshow

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //pin-generate 
 document.getElementById('pin-generate').addEventListener('click', function(){
-   
+   const pin = Math.round(Math.random()*100000)
+   const pinShow = document.getElementById("pin-show");
+   pinShow.value = pin;
 })
