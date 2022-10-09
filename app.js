3795ae531acbc2e1fecee20ff50837e1703cd116
Rasl0406 <durjoychowdhury0406@gmail.comm>
2022-10-10 01:31:59 +0600

    calculator Number Show

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ function getPin() {
         return getPin();
     }
 }
-
 document.getElementById('pin-generate').addEventListener('click', function(){
    const pin = getPin();
    const pinShow = document.getElementById("pin-show");
    pinShow.value = pin;
+})
+
+// calculator 
+document.getElementById('key-pad').addEventListener('click', function(event){
+   const eventNumber = event.target.innerText
+   const eventNumberShow = document.getElementById("number-display");
+   eventNumberShow.value = eventNumber;
+   
 })
