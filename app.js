9e56e021609ff3a68fd22a4fc678f28bf95a9b9d
Rasl0406 <durjoychowdhury0406@gmail.comm>
2022-10-09 17:53:44 +0600

    get pin

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 //pin-generate 
+function getPin() {
+    const pin = Math.round(Math.random()*100000);
+     const pinNumber = pin + '';
+     if (pinNumber.length == 4) {
+        return pinNumber;
+     }
+     else{
+        return getPin();
+    }
+}
+
 document.getElementById('pin-generate').addEventListener('click', function(){
-   const pin = Math.round(Math.random()*100000)
+   const pin = getPin();
    const pinShow = document.getElementById("pin-show");
    pinShow.value = pin;
 })
